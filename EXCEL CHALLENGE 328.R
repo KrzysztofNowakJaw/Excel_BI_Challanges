5f1c6468a57fb7d400d0bcdf368a1ccb83c325a9
KrzysztofNowakJaw <knowak1990@outlook.com>
2023-11-18 15:57:44 +0100

    Added comments

--- EXCEL CHALLENGE 328.R
+++ EXCEL CHALLENGE 328.R
@@ -1,27 +1,28 @@
+# Load required libraries
 library(tidyverse)
 library(readxl) 
 
-df <- read_xlsx("Remove Minimum.xlsx",range = "A1:A7")
+# Read data from Excel file
+df <- read_xlsx("Remove Minimum.xlsx", range = "A1:A7")
 
-
+# Define a function to remove the last occurrence of the minimum value in a comma-separated string
 RemoveLastMin <- function(x) {
   
-  NumberConversion <- as.numeric(unlist(str_split(x,",")))
+  # Convert the comma-separated string to a numeric vector
+  NumberConversion <- as.numeric(unlist(str_split(x, ",")))
   
-  min_value <- min(NumberConversion)
+  # Find the index of the minimum value
+  min_index <- which.min(NumberConversion)
   
-  which(NumberConversion == min_value)
+  # Remove the last occurrence of the minimum value
+  RemoveIndex <- NumberConversion[-min_index]
   
-  last_index <- tail(which(NumberConversion == min_value), 1)
+  # Convert the modified vector back to a string
+  result <- paste(unlist(str_split(RemoveIndex, " ")), collapse = ",")
   
-  RemoveIndex <- NumberConversion[-last_index]
-  
-  result <- paste(unlist(str_split(RemoveIndex," ")),collapse = ",")
-  
+  # Print the result
   print(result)
 }
 
-
-Answer <- df$Answer <- sapply(df$String,RemoveLastMin)
-
-df
+# Apply the RemoveLastMin function to each element in the 'String' column and store the results in the 'Answer' column
+df$Answer <- sapply(df$String, RemoveLastMin)
