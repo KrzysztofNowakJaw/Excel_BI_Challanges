1dc667ff3c564c9c7f02287b201a5c85daa32d7e
KrzysztofNowakJaw <knowak1990@outlook.com>
2023-12-11 11:10:46 +0100

    Version with diff

--- EXCEL CHALLENGE 344.R
+++ EXCEL CHALLENGE 344.R
@@ -7,20 +7,16 @@ Numbers <- c(32,989,40021,43210,764321,1906368,98765321,903363631,9988776655)
 
 df <- data.frame(Numbers = Numbers)
 
-IsKatadrome <- function(x) {
+IsKatadromeTwo <- function(x) {
   
   x <- as.character(x)
-  Split <- sort(str_split(x,"")[[1]],decreasing = TRUE)
-  IsUnique <- sum(duplicated(Split)) == 0
-  IsSorted<- paste(sort(str_split(x,"")[[1]],decreasing = TRUE),collapse = "") == x 
-  IsUnique & IsSorted
-
+  digits <- as.numeric(unlist(str_split(x,"")))
+  return(all(diff(digits) < 0 ))
 }
   
 Answer <- df |>
   rowwise() |>
-  filter(IsKatadrome(Numbers) == TRUE)
+  filter(IsKatadromeTwo(Numbers) == TRUE)
 
-Answer 
-
+Answer
 
