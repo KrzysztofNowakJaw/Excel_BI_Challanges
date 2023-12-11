f623b5b417c490af998e32c9522665406108e76d
KrzysztofNowakJaw <knowak1990@outlook.com>
2023-12-11 10:01:59 +0100

    Link added

--- EXCEL CHALLENGE 344.R
+++ EXCEL CHALLENGE 344.R
@@ -1,3 +1,6 @@
+#Link to challenge
+#https://www.linkedin.com/search/results/content/?fromMember=%5B%22ACoAAByBXHwBWbdpEpS1fnfvxD21zkOGmmhNQWU%22%5D&heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAAByBXHwBWbdpEpS1fnfvxD21zkOGmmhNQWU&keywords=Excel%20BI&sid=F5l&update=urn%3Ali%3Afs_updateV2%3A(urn%3Ali%3Aactivity%3A7139826139807608832%2CBLENDED_SEARCH_FEED%2CEMPTY%2CDEFAULT%2Cfalse)
+
 library(tidyverse)
 
 Numbers <- c(32,989,40021,43210,764321,1906368,98765321,903363631,9988776655)
@@ -14,12 +17,10 @@ IsKatadrome <- function(x) {
 
 }
   
-df |>
+Answer <- df |>
   rowwise() |>
   filter(IsKatadrome(Numbers) == TRUE)
 
-#OR
-
-keep(Numbers,IsKatadrome)
+Answer 
 
 
