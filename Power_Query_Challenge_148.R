e8b6fa7b8b66d5d03d2d0ed08011e8e6089688fe
„KrzysztofNowakJaw” <knowak1990@outlook.com>
2024-01-14 13:51:03 +0100

    Amended

---
+++ Power_Query_Challenge_148.R
@@ -0,0 +1,21 @@
+#Link To Challenge
+#https://www.linkedin.com/feed/update/urn:li:activity:7152147318102728705/
+
+library(tidyverse)
+library(readxl)
+
+df <- read_xlsx("Power_Query_Challenge_148.xlsx",range = "A1:A12")
+Expected <- read_xlsx("Power_Query_Challenge_148.xlsx",range = "C1:N12")
+
+
+Answer <- df |>
+  separate_longer_delim(Fruits,", ") |>
+  group_by(Fruits) |>
+  mutate(Count = n()) |>
+  arrange(Fruits) |>
+  unique() |>
+  mutate(Headers = Fruits) |>
+  pivot_wider(id_cols = Fruits,names_from = Headers,values_from = Count)
+
+
+Answer
