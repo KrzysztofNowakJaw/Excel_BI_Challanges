fc9fb9b5eb1683e6fed3f56d47b4ec9c40088a74
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2024-05-08 13:14:18 +0200

    Add files via upload

---
+++ Excel_Challenge_449 - Rotated Strings.R
@@ -0,0 +1,21 @@
+#Link to challange
+#https://www.linkedin.com/posts/excelbi_excel-challenge-problem-activity-7193097156491100160-pmwm?utm_source=share&utm_medium=member_desktop
+
+library(tidyverse)
+library(readxl)
+
+
+filename <- "Excel_Challenge_449 - Rotated Strings.xlsx"
+
+df <- read_xlsx(filename, range = "A1:B10") 
+
+CheckRotation <- function(x,y) {
+
+Extended <- str_c(x,x)
+Check <- str_detect(Extended,y) & x != y
+return(Check)
+}
+
+Answer <- df |>
+  filter(map2_lgl(String1,String2,CheckRotation))
+
