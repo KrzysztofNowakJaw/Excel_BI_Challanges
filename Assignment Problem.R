f03ebc50ff6c0f45d1566f04425790fbb37f3403
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2024-05-09 11:01:41 +0200

    Update Assignment Problem.R

--- Assignment Problem.R
+++ Assignment Problem.R
@@ -1,40 +1,29 @@
+#LinkToChallange
+#https://www.linkedin.com/posts/omid-motamedisedeh-74aba166_excelchallenge-powerquerychllenge-excel-activity-7194078630744842240-MlNb?utm_source=share&utm_medium=member_desktop
+
 library(tidyverse)
 library(readxl)
 
 filename <- "CH-049 Assignment Problem Part 1.xlsx"
 
-df <- read_xlsx(filename, range = "B2:F6") |> janitor::clean_names()
+df <- read_xlsx(filename, range = "B2:F6") 
 
-RowPart <- df |>
+RowWise <- df |>
   rowwise() |>
   mutate(Lowest = min(c_across(where(is.numeric))),
-         across(starts_with("task"),\(x) x -Lowest)) |>
+         across(starts_with("task"),\(x) x - Lowest)) |>
   select(-c(Lowest)) |>
   ungroup()
 
-ColMin <- apply(RowPart[2:ncol(RowPart)], 2, min)
-
-Combined <- bind_rows(RowPart,ColMin) |>
-  mutate(across(where(is.numeric),\(x) paste(x,collapse = ",")))
-
-MojaF <- function(x) {
-  
-  MyList <- x
-  Split <- as.numeric(str_split(MyList,",")[[1]]) 
-  RemoveLast <- Split[1:length(Split)-1]
-  Deductor <- Split[length(Split)]
-  ApplyDeduct <- lapply(RemoveLast, function (x) {x - Deductor})
-  Result <- paste(ApplyDeduct,collapse = ",")
-  return(Result)
+DeductMin <- function(x) {
+  x - min(x) 
 }
 
-Deducted <- apply(Combined[2:ncol(Combined)], 2, MojaF) |> as_tibble()
-
-Step2 <- Deducted |>
-  mutate(Index = row_number(),.before = value) |>
-  pivot_wider(names_from = Index,values_from = value,names_prefix = "Task_") |>
-  separate_longer_delim(everything(),delim = ",")
+Step2 <- apply(RowWise[2:ncol(RowWise)],2,DeductMin) |> as_tibble()
 
 Answer <- df |>
   select(1) |>
   bind_cols(Step2)
+
+Answer |>
+  gt::gt()
