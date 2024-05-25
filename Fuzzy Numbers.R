60c8adf5f027afed3bdc1d82ef1a32243bdfcbd6
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2024-05-25 11:55:36 +0200

    Add files via upload

---
+++ Fuzzy Numbers.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(readxl)
+
+filename <- "CH-57 Fuzzy Numbers Calculation.xlsx"
+
+df <- read_xlsx(filename, range = "B2:C15")
+Expected <- read_xlsx(filename, range = "G2:H15")
+
+Adding <- function(x, y, Sign = "+") {
+  A <- as.numeric(str_extract_all(x, "\\d+")[[1]])
+  B <- as.numeric(str_extract_all(y, "\\d+")[[1]])
+  MatrixA <- matrix(A)
+  MatrixB <- matrix(B)
+
+  # Perform the operation based on the Sign argument
+  result <- switch(Sign,
+    "+" = MatrixA + MatrixB,
+    "-" = c(MatrixA[1] - MatrixB[3], MatrixA[2] - MatrixB[2], MatrixA[3] - MatrixB[1]),
+    stop("Invalid operator specified.")
+  )
+
+  Add <- paste(result, collapse = ",")
+  return(Add)
+}
+
+Answer <- df |>
+  mutate(
+    Adding = map2_chr(A, B, ~ Adding(.x, .y, Sign = "+")),
+    Substracting = map2_chr(A, B, ~ Adding(.x, .y, Sign = "-"))
+  )
+
+Answer |>
+  bind_cols(Expected) |>
+  mutate(Test = Adding == `A+B` & Substracting == `A-B`)
