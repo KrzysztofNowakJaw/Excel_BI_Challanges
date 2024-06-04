bb8cfa0502fa28d8d544170f82b4c52fa7cae12d
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2024-06-04 10:40:05 +0200

    Update Linear interpolation.R

--- Linear interpolation.R
+++ Linear interpolation.R
@@ -1,12 +1,34 @@
+#Link to challange:
+#https://www.linkedin.com/posts/omid-motamedisedeh-74aba166_excelchallenge-powerquerychllenge-excel-activity-7203500706681479168-hibJ?utm_source=share&utm_medium=member_desktop
+
+#We can solve this problem in 2 ways. First involve zoo::na.approx() which makes in almost automatical. 
+#Second involve only tidyverse and requre more data wrangling
+
+#Solution with {zoo}
+
+df <- read_xlsx(filename, range = "B2:D21", col_types = c("date", "text", "numeric"))
+Expected <- read_xlsx(filename, range = "H2:J38", col_types = c("date", "text", "numeric")) |> 
+  mutate(`Actual Progress` = `Actual Progress`)
+
+Base <- df |>
+  mutate(
+    Date = floor_date(Date, unit = "month")
+  ) |>
+  group_by(Project) |>
+  complete(Date = seq.Date(from = min(as.Date(Date)), to = max(as.Date(Date)), by = "1 month")) |>
+  mutate(Index = row_number()) 
+  
+
+Answer <- Base |>
+  mutate(`Actual Progress` = zoo::na.approx(`Actual Progress`))
+
+
+#Data wrangling solution
+
 library(dplyr)
 library(tidyr)
 library(lubridate)
 library(scales)
-
-filename <- "CH-062 Missing Values.xlsx"
-
-df <- read_xlsx(filename, range = "B2:D21", col_types = c("date", "text", "numeric"))
-Expected <- read_xlsx(filename, range = "H2:J38", col_types = c("date", "text", "numeric")) |> mutate(`Actual Progress` = percent(`Actual Progress`))
 
 Base <- df |>
   mutate(
