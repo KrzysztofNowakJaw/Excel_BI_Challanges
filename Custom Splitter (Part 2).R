588f88d2ac61952db83f2ef8eb63028e87516194
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2024-06-25 23:38:32 +0200

    Add files via upload

---
+++ Custom Splitter (Part 2).R
@@ -0,0 +1,27 @@
+#link to challenge
+#https://www.linkedin.com/posts/omid-motamedisedeh-74aba166_excelchallenge-powerquerychllenge-excel-activity-7211473239376637954-5PJK?utm_source=share&utm_medium=member_desktop
+
+library(tidyverse)
+library(readxl)
+
+filename <- "CH-073 Custom splitter 2.xlsx"
+
+df <- read_xlsx(filename, range = "B2:B15") 
+Expected <- read_xlsx(filename, range = "D2:F24",col_types = c("text","text","text"))
+
+Date_pattern <- "^\\d{4}\\/\\d{1,2}\\/\\d{1,2}"
+
+Answer <- df |>
+  mutate(Date = str_extract(Info,Date_pattern),
+         Info = str_remove(Info,Date_pattern),
+         Values = str_extract_all(Info,"[[:alpha:]]+\\d+"))  |>
+  unnest_longer(Values) |>
+  mutate(Product = str_extract(Values,"^[[:alpha:]]+"),
+         Quantity = str_extract(Values,"\\d+$")) |>
+  select(names(Expected))
+
+Answer
+
+all.equal(Expected,Answer)
+
+
