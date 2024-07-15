93b470ed5a3b24231a3bc2cf7b66e09fb3ba8de5
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2024-07-15 10:58:13 +0200

    Add files via upload

---
+++ PQ_Challenge_199.R
@@ -0,0 +1,29 @@
+#Link to challange
+#https://www.linkedin.com/posts/excelbi_challenge-powerquerychallenge-daxchallenge-activity-7217739351147872256-3VjL?utm_source=share&utm_medium=member_desktop
+
+library(tidyverse)
+library(readxl)
+
+filename <- "PQ_Challenge_199.xlsx"
+
+x <- "part no 978 to be sent on 12/28/23 and 11/2/23"
+
+df <- read_xlsx(filename, range = "A1:A5")
+
+StructureValue <- function(x) {
+  datepattern <- "\\d+[[:punct:]]+\\d+[[:punct:]]+\\d+"
+
+  tibble(
+    Date = str_extract_all(x, pattern = datepattern),
+    `Part No.` = str_extract_all(x, pattern = "\\d{3,}")
+  ) |> unnest_longer(everything())
+}
+
+Combined <- map_dfr(df, StructureValue)
+
+Answer <- Combined |>
+  mutate(Date = str_replace_all(Date, "[[:punct:]]{2,}", "/\\")) |>
+  mutate(Date = as.Date(Date, format = "%m/%d/%y")) |>
+  arrange(`Part No.`, Date)
+
+Answer
