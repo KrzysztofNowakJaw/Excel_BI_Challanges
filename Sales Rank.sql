a0574b39f0dfd6d95f42f50bde97c9bb1e45eac0
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2024-08-12 11:06:04 +0200

    Add files via upload

---
+++ Sales Rank.sql
@@ -0,0 +1,46 @@
+WITH Sums AS (
+    SELECT 
+        product,
+        date_part('month', date) AS Month,
+        SUM(quantity) OVER (PARTITION BY date_part('month', date) ORDER BY date_part('month', date)) AS Monthly,
+        SUM(quantity) OVER (PARTITION BY date_part('month', date), product) AS MonthlyProduct
+    FROM 
+        Sales96
+),
+
+ranks AS (
+    SELECT 
+        product,
+        month,
+        Monthly,
+        MonthlyProduct,
+        DENSE_RANK() OVER (PARTITION BY month ORDER BY MonthlyProduct DESC) AS SalesRank
+    FROM 
+        Sums
+),
+
+renamed AS (
+    SELECT DISTINCT
+        month,
+        product,
+        CASE 
+            WHEN SalesRank > 2 THEN 'Other' 
+            ELSE product  
+        END AS ProductNew,
+        MonthlyProduct::FLOAT,
+        Monthly::FLOAT,
+        SalesRank
+    FROM 
+        ranks
+)
+
+SELECT 
+    month,
+    productnew,
+    (SUM(monthlyproduct) / monthly * 100)::INT || '%' AS Percentage
+FROM 
+    renamed
+GROUP BY 
+    month, monthly, productnew
+ORDER BY 
+    month,LENGTH(productnew) 
