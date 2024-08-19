690812c5e3b30f125eb405af54fcb994214ac524
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2024-08-19 23:57:05 +0200

    Update Duplicate Values No.100.R

--- Duplicate Values No.100.R
+++ Duplicate Values No.100.R
@@ -7,15 +7,15 @@ product_ids <- c(100, 100, 102, 103, 107, 107, 107, 107, 108, 108, 108, 108, 109
 df <- data.frame(Product_ID = product_ids)
 
 Answer <- df |>
-  summarise(Count = n(), .by = Product_ID) |>
   group_by(Product_ID) |>
+  summarise(Count = n()) |>
   complete(Count = seq(from = 1, to = Count, by = 1)) |>
   mutate(IsDup = n(),
-    Result =
-      ifelse(IsDup > 1,
-        str_c(as.character(Product_ID), LETTERS[Count], sep = ""),
-        as.character(Product_ID)
-      )
+         Result =
+           ifelse(IsDup > 1,
+                  str_c(as.character(Product_ID), LETTERS[Count], sep = ""),
+                  as.character(Product_ID)
+           )
   )
 
 data.table::data.table(Answer)
