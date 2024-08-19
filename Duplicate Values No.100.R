5c30716aded1875f0e7d1f454a56138d0210eb91
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2024-08-20 00:01:47 +0200

    Update Duplicate Values No.100.R

--- Duplicate Values No.100.R
+++ Duplicate Values No.100.R
@@ -7,8 +7,8 @@ product_ids <- c(100, 100, 102, 103, 107, 107, 107, 107, 108, 108, 108, 108, 109
 df <- data.frame(Product_ID = product_ids)
 
 Answer <- df |>
+  summarise(Count = n(),.by = Product_ID) |>
   group_by(Product_ID) |>
-  summarise(Count = n()) |>
   complete(Count = seq(from = 1, to = Count, by = 1)) |>
   mutate(IsDup = n(),
          Result =
@@ -19,4 +19,3 @@ Answer <- df |>
   )
 
 data.table::data.table(Answer)
-
