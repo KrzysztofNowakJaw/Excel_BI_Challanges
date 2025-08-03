3738cf6901859fc14744d3acea719e0454b19840
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2025-08-03 10:22:02 +0200

    Add files via upload

---
+++ POWER QUERY CHALLENGE 310.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(readxl)
+
+
+df <- read_xlsx(File_name,range = "A1:D13")
+
+Answer <- df |>
+  fill(everything(), .direction = "down") |>
+  group_by(Name) |>
+  mutate(Index = row_number()) |>
+  fill(everything(), .direction = "up") |>
+  filter(Index == max(Index)) |>
+  select(-Index)
+
+Answer
