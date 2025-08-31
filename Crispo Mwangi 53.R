c6b8df6c39adcf364a41f56adc17f8ce9ddb198a
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2025-08-31 20:22:38 +0200

    Crispo MwangiCrispo Mwangi

---
+++ Crispo Mwangi 53.R
@@ -0,0 +1,15 @@
+#https://crispomwangi-my.sharepoint.com/:x:/g/personal/crispo_crispexcel_com/EfeVtNNxSrRFtaESuxw_jvIBR70LBPtd_zatocMbR22KHQ?e=baelSp
+library(tidyverse)
+
+#Define dataset source
+df <- read_xlsx(File_name,range = "B2:E7")
+
+Answer <- df |>
+  rowwise() |> #apply below functions per row
+    mutate( #Create new column
+    across(2:ncol(df), \(x) !is.na(x)), # for every row-column combination after ID return 1 for non NA and 0 for NA
+    SumNa = sum(c_across(2:ncol(df)))) |> #Sum NonNA
+  filter(SumNa == 0) |> #Filter for 0 sum
+  select(1) #Select 1 column
+
+Answer
