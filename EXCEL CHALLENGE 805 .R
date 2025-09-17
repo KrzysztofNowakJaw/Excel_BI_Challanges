428853c5b323d8e21dc47d7cdb6f41f6f3936152
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2025-09-17 12:17:13 +0200

    change

--- EXCEL CHALLENGE 805 .R
+++ EXCEL CHALLENGE 805 .R
@@ -1,9 +1,11 @@
 #https://www.linkedin.com/posts/excelbi_excel-challenge-problem-activity-7373566317884411904-Y0Xw?utm_source=share&utm_medium=member_desktop&rcm=ACoAACOAYFUBiUaa9Y4mEwqoxo7zB0wNDVUrSg0
 
+df <- read_xlsx("/Users/krzysztofnowak/Desktop/ExcelBi/Excel_BI_Challanges/Excel_Challenge_805 - Vowels in Increasing or Decreasing Order.xlsx",range = "A2:B16")
+
+
 library(tidyverse)
 library(readxl)
 
-df <- read_xlsx("Excel_Challenge_805 - Vowels in Increasing or Decreasing Order.xlsx",range = "A2:B16")
 
 VowelsOrder <- function(word) {
 
