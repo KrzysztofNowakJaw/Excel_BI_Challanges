e828119f049f8be6a81964ad28a97d09187a38fa
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2026-02-23 22:30:26 +0100

    dsds

---
+++ CH-372 Filter.R
@@ -0,0 +1,21 @@
+#https://docs.google.com/spreadsheets/d/1U6WDXfzksGr4zm4fZBSaUpBKjp7d0QW0/edit?gid=493106675#gid=493106675
+
+library(tidyverse)
+
+df <- tibble(
+  ID = c(
+    "MN12Q",
+    "MNMN!Q",
+    "F12312Q1",
+    "UMNMQUNMM",
+    "MMQN89",
+    "QQQ",
+    "MNNUM"
+  )
+)
+
+df |>
+  mutate(Numbers = str_extract_all(ID, '\\d{1}')) |>
+  unnest(cols = Numbers) |>
+  filter(as.numeric(Numbers) %% 2 == 0) |>
+  distinct()
