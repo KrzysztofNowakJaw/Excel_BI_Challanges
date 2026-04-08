aad59323af8993b0cb265c661f5d3615b20420a0
“Krzysztof <knowak1990@outlook.com>
2026-04-08 12:55:01 +0200

    anagrams

---
+++ Excel_Challenge_951 - Grouping Anagrams  .R
@@ -0,0 +1,29 @@
+#https://onedrive.live.com/:x:/g/personal/e11b26eeaacb7947/IQD2zXBVNsVqToND6h04yg7EASlpKVHC8F5HhOvJISJZ9zM?rtime=nyYh31OV3kg&redeem=aHR0cHM6Ly8xZHJ2Lm1zL3gvYy9lMTFiMjZlZWFhY2I3OTQ3L0lRRDJ6WEJWTnNWcVRvTkQ2aDA0eWc3RUFTbHBLVkhDOEY1SGhPdkpJU0paOXpNP2U9QUtDQWJE
+
+library(tidyverse)
+library(readxl)
+
+
+df <- read_xlsx(File_name, range = "A1:A23")
+
+Answer <- df |>
+  cross_join(df) |>
+  filter((Data.x != Data.y) & nchar(Data.x) == nchar(Data.y)) |>
+  rowwise() |>
+  mutate(
+    X_Split = list(str_split_1(Data.x, "")),
+    Y_Split = list(str_split_1(Data.y, ""))
+  ) |>
+  filter(length(setdiff(X_Split, Y_Split)) == 0) |>
+  select(contains('Data')) |>
+  group_by(Data.x) |>
+  reframe(Anagrams = list(Data.y)) |>
+  rowwise() |>
+  mutate(
+    Anagrams = list(sort(append(Data.x, Anagrams))),
+    Anagrams = paste(Anagrams, collapse = ", ")
+  ) |>
+  select(Anagrams) |>
+  unique()
+
+Answer
