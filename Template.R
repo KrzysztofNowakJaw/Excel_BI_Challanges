77237435d5076a55730bdf5bc57ba70ee4c6a95f
“Krzysztof <knowak1990@outlook.com>
2026-04-14 18:16:44 +0200

    template

--- Template.R
+++ Template.R
@@ -1,31 +1,4 @@
-#Download recent excel file to project folder
-
-library(fs)
-library(tidyverse)
-library(readxl)
-
-File_To_Move <- fs::dir_info(path = '/Users/krzysztofnowak/Downloads') |>
-  arrange(desc(modification_time)) |>
-  filter(str_detect(path, '^.+\\.xlsx')) |>
-  head(1) |>
-  select(path)
-
-
-fs::file_move(File_To_Move[[1]], getwd())
-
-
-File_name <- str_extract(File_To_Move, '(?<=Downloads\\/).+.xlsx$')
+source(file = 'EBI_Load.R')
+source(file = 'EBI_remove.R')
 
 read_xlsx(File_name)
-
-File_name
-
-# Remove the file after reading
-Files <- fs::dir_info(path = '~/Desktop/Excel_BI_Challanges') |>
-  arrange(desc(modification_time)) |>
-  filter(str_detect(path, '^.+\\.xlsx$')) |>
-  select(path)
-
-lapply(Files, FUN = fs::file_delete)
-
-rm(list = ls())
